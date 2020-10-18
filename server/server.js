ee70a5de83e7a122c9d1709087e5fff40055c0a0
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-18 10:40:20 -0700

    installed apollo-server-express & created schema directory

--- server/server.js
+++ server/server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const path = require('path');
+//import ApolloSever
+//authentication middleware
+
+//import typeDefs and resolvers
 const db = require('./config/connection');
 const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//create a new Apollo server and pass in our schema data
+
+//integrate our Apollo server with the express apllication middleware
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
