237ba5229f7518337397de7da1268803863c2f67
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-18 11:34:51 -0700

    resolvers - created me query and addUser, Login mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,5 +1,47 @@
+//import User model
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signtoken, signToken } = require("../utils/auth");
+
 const resolvers = {
-    
-  };
-  
-  module.exports = resolvers;
+  //Query
+  Query: {
+    me: async (parent, args, context) => {
+        if (context.user) {
+            const userData = await User.findOne({ _id: context.user._id })
+            .select('-__v -password');
+
+            return userData;
+        }
+
+        throw new AuthenticationError('Not logged in');
+    },
+  },
+
+  //Mutations
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError("Incorrect credentials");
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
+};
+
+module.exports = resolvers;
