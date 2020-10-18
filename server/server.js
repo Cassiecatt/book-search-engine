7c1eafebefd4ad7745edf7d612ab8e4fca120f14
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-18 10:59:52 -0700

    imported auth file to server.js

--- server/server.js
+++ server/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const path = require('path');
 //import ApolloSever
+const { ApolloServer } = require('apollo-server-express');
 //authentication middleware
+const { authMiddleware } = require('./utils/auth');
 
 //import typeDefs and resolvers
+const { typeDefs, resolvers } = ('./schemas');
 const db = require('./config/connection');
 const routes = require('./routes');
 
@@ -11,8 +14,14 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 //create a new Apollo server and pass in our schema data
+const server = new ApolloServer({
+  typeDefs, 
+  resolvers,
+  context: authMiddleware
+});
 
-//integrate our Apollo server with the express apllication middleware
+//integrate our Apollo server with the express application middleware
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -26,4 +35,5 @@ app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
