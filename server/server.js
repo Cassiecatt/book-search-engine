0d8d9b93c2a4f4ce61777dd7bca6cb6f071377f5
Cassiecatt <65003865+Cassiecatt@users.noreply.github.com>
2020-10-18 21:39:10 -0700

    fixed server error

--- server/server.js
+++ server/server.js
@@ -1,29 +1,26 @@
 const express = require('express');
-const path = require('path');
-//import ApolloSever
-const { ApolloServer } = require('apollo-server-express');
-//authentication middleware
-const { authMiddleware } = require('./utils/auth');
-
-//import typeDefs and resolvers
-const { typeDefs, resolvers } = ('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
+//import ApolloServer
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+const { typeDefs, resolvers } = require('./schemas');
+//import auth middleware
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
-  typeDefs, 
+  typeDefs,
   resolvers,
-  context: authMiddleware
+  context: authMiddleware,
 });
 
 //integrate our Apollo server with the express application middleware
 server.applyMiddleware({ app });
 
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
@@ -31,13 +28,15 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// app.use(routes);
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
-
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
